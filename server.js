eea74c2c454e2c53cae85383f994254ac4767fe0
Ankita Dash <ankita.iu07@gmail.com>
2016-05-07 23:26:09 -0700

    adding server

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var app = express();
-
-var PORT = process.env.PORT || 4001 // env variable 
-
+var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Look for NodeJS jobs',
+	completed: false
+}, {
+	id: 2,
+	description: 'Go to market',
+	completed: false
+}, {
+	id: 3,
+	description: 'Apply online',
+	completed: true
+}];
 
 app.get('/', function (req, res) {
-resp.send('Todo API Root');
-
-
+	res.send('Todo API Root');
 });
 
+// GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+// GET /todos/:id
+
+
 app.listen(PORT, function () {
-console.log('Express listening on Port' + PORT +'!');
-
-
+	console.log('Express listening on port ' + PORT + '!');
 });
